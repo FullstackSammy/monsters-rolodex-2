888679d9794cedeb8559b254fa7b8e2e333622b0
samzzii <nasstromsamuel@icloud.com>
2023-10-29 23:44:44 +0100

    Added searcbhar for the project and filtered the list based on event.target.value

--- src/App.js
+++ src/App.js
@@ -9,26 +9,39 @@ class App extends Component {
     this.state = {
       monsters: [],
     };
+    console.log("constructor");
   }
 
   componentDidMount() {
+    console.log("didMount");
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((users) =>
-        this.setState(
-          () => {
-            return { monsters: users };
-          },
-          () => {
-            console.log(this.state);
-          }
-        )
+        this.setState(() => {
+          return { monsters: users };
+        })
       );
   }
 
   render() {
+    console.log("render");
     return (
       <div className="App">
+        <input
+          className="search-box"
+          type="search"
+          placeholder="search monster"
+          onChange={(event) => {
+            const searchString = event.target.value.toLowerCase();
+            const filteredMonsters = this.state.monsters.filter((monster) => {
+              return monster.name.toLowerCase().includes(searchString);
+            });
+
+            this.setState(() => {
+              return { monsters: filteredMonsters }
+            })
+          }}
+        />
         {this.state.monsters.map((monster) => {
           return (
             <div key={monster.id}>
