23ec07bd6c6566c86f71db9a38061a087648dd54
samzzii <nasstromsamuel@icloud.com>
2023-10-30 11:59:47 +0100

    added filtered search

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
 
     this.state = {
       monsters: [],
+      searchField: "",
     };
     console.log("constructor");
   }
@@ -25,6 +26,11 @@ class App extends Component {
 
   render() {
     console.log("render");
+
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(this.state.searchField);
+    });
+
     return (
       <div className="App">
         <input
@@ -32,17 +38,13 @@ class App extends Component {
           type="search"
           placeholder="search monster"
           onChange={(event) => {
-            const searchString = event.target.value.toLowerCase();
-            const filteredMonsters = this.state.monsters.filter((monster) => {
-              return monster.name.toLowerCase().includes(searchString);
-            });
-
+            const searchField = event.target.value.toLowerCase();
             this.setState(() => {
-              return { monsters: filteredMonsters }
-            })
+              return { searchField };
+            });
           }}
         />
-        {this.state.monsters.map((monster) => {
+        {filteredMonsters.map((monster) => {
           return (
             <div key={monster.id}>
               <h1>{monster.name}</h1>
