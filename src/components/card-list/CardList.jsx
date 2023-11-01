bcbacfd20271f10ca77fdf331420ba304dd33453
samzzii <nasstromsamuel@icloud.com>
2023-11-01 23:12:25 +0100

    made some optimizations for the cardlist component

--- src/components/card-list/CardList.jsx
+++ src/components/card-list/CardList.jsx
@@ -1,23 +1,30 @@
-import React, { Component } from 'react';
-import './CardList.css'
+import React, { Component } from "react";
+import "./CardList.css";
 
 class CardList extends Component {
-    render() {
-        console.log('render from card list');
-        const { monsters } = this.props;
-        
-        return (
-            <div className='card-list'>
-            {monsters.map((monster) => (
-                <div className='card-container' key={monster.id}>
-                <img alt='monster'src={`https://robohash.org/${monster.id}?set=set2&size=180x180`}/>
-                <h2>{monster.name}</h2>
-                <p>{monster.email}</p>
-                </div>
-            ))}
+  render() {
+    console.log("render from card list");
+    const { monsters } = this.props;
+
+    return (
+      <div className="card-list">
+        {monsters.map((monster) => {
+          const { name, email, id } = monster;
+
+          return (
+            <div className="card-container" key={id}>
+              <img
+                alt="monster"
+                src={`https://robohash.org/${id}?set=set2&size=180x180`}
+              />
+              <h2>{name}</h2>
+              <p>{email}</p>
             </div>
-        );
-    }
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default CardList;
