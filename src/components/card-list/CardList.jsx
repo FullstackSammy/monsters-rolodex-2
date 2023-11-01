a9fb5bf6aed59784e24a5c750a312f41ab00d80a
samzzii <nasstromsamuel@icloud.com>
2023-11-01 23:06:51 +0100

    added styling for cards and cardlist

--- src/components/card-list/CardList.jsx
+++ src/components/card-list/CardList.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './CardList.css'
 
 class CardList extends Component {
     render() {
@@ -6,10 +7,12 @@ class CardList extends Component {
         const { monsters } = this.props;
         
         return (
-            <div>
+            <div className='card-list'>
             {monsters.map((monster) => (
-                <div key={monster.id}>
-                <h1>{monster.name}</h1>
+                <div className='card-container' key={monster.id}>
+                <img alt='monster'src={`https://robohash.org/${monster.id}?set=set2&size=180x180`}/>
+                <h2>{monster.name}</h2>
+                <p>{monster.email}</p>
                 </div>
             ))}
             </div>
